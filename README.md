480d4d37fb86fef29549fbd098994dc443fae2e4
Sinaev Evgenii <evgenii.sinaev@orioninc.com>
2023-03-23 13:12:30 +0100

    New Commit 1. Added input of token with keyboard 2. Added launch instruction

---
+++ README.md
@@ -0,0 +1,6 @@
+**How to launch JAR-file**
+
+Need to generate new TOKEN with GitHub-account \
+Get TOKEN here: https://www.toptal.com/developers/hastebin/documentation \
+Enter the command in any terminal: **java -jar ServiceAPI-1.0-SNAPSHOT-jar-with-dependencies.jar**** \
+Enter your token in the field
--- src/main/java/org/orioninc/HastebinService.java
+++ src/main/java/org/orioninc/HastebinService.java
@@ -3,23 +3,44 @@ package org.orioninc;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class HastebinService {
-    private final static String BEARER_TOKEN_HASTEBIN = "Bearer " + System.getenv("TOKEN_HASTEBIN");
+    private final static String BEARER_TOKEN_HASTEBIN = "Bearer " + System.getenv("token");
     private final static String API_ENDPOINTS_HASTEBIN = "https://hastebin.com";
     private final static String POST_ROUTE_HASTEBIN = "/documents";
 
+    private String inputToken(String newToken) {
+        String enterToken;
+        System.out.print("Enter token: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            enterToken = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (enterToken.isEmpty()){
+            newToken = BEARER_TOKEN_HASTEBIN;
+        } else {
+            newToken = "Bearer " + enterToken;
+        }
+        return newToken;
+    }
     public void submitDocument(String allStringQuestions) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+        String newToken = null;
+        newToken = inputToken(newToken);
+
         HttpRequest requestPost = HttpRequest.newBuilder()
                 .uri(URI.create(API_ENDPOINTS_HASTEBIN + POST_ROUTE_HASTEBIN))
                 .header("Content-Type", "text/plain; charset=UTF-8")
-                .header("Authorization", BEARER_TOKEN_HASTEBIN)
+                .header("Authorization", newToken)
                 .POST(HttpRequest.BodyPublishers.ofString(allStringQuestions))
                 .build();
 
