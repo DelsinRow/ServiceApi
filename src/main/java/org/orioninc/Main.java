c2e8910c55544d6b152568de89fbfeef3dcc9621
Evgenii Sinaev <evgenii.sinaev@mera.com>
2023-05-17 12:58:07 +0200

    added ServiceManager class

--- src/main/java/org/orioninc/Main.java
+++ src/main/java/org/orioninc/Main.java
@@ -1,52 +1,21 @@
 package org.orioninc;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
-    private static QuestionsSubmitter questionsSubmitter;
     public static void main(String[] args) throws IOException, InterruptedException {
-        String key;
-        HttpClient client = HttpClient.newHttpClient();
         final long start = System.nanoTime();
-        List<Questions> allQuestionsList = new ArrayList<>();
+        String key;
+        List<Questions> allQuestionList;
+        QuestionsSubmitter questionsSubmitter;
 
-        StackOverflowService stackOverflowService = new StackOverflowService(client);
+        ServiceManager serviceManager = new ServiceManager();
 
-        List<CompletableFuture<Questions>> listOfCompletableFutureOfQuestions = new ArrayList<>();
-        for (int i = 0; i < args.length; i++) {
-            for (Languages language : Languages.values()) {
-                if (args[i].equalsIgnoreCase(language.getLanguageName())) {
-                    listOfCompletableFutureOfQuestions.add(stackOverflowService.sendRequest(language));
-                }
-            }
-        }
-        CompletableFuture.allOf(listOfCompletableFutureOfQuestions.toArray(CompletableFuture[]::new));
-
-        listOfCompletableFutureOfQuestions.forEach(f -> {
-            try {
-                allQuestionsList.add(f.get());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        if (System.getenv("SERVICE").equals("storage")) {
-            questionsSubmitter = new StorageService(client);
-        } else if (System.getenv("SERVICE").equals("hastebin")) {
-            questionsSubmitter = new HastebinService(client);
-        } else {
-            System.out.println("ERROR. Unknown service: " + System.getenv("SERVICE"));
-            System.exit(1);
-        }
-        key = questionsSubmitter.submitDocument(allQuestionsList);
+        questionsSubmitter = serviceManager.verifyAndChoiceService();
+        allQuestionList = serviceManager.allQuestionsList(args);
+        key = questionsSubmitter.submitDocument(allQuestionList);
         questionsSubmitter.printFinalLink(key);
 
         System.out.printf("Done in %dms\n", Duration.ofNanos(System.nanoTime() - start).toMillis());
